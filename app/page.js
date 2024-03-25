c0aaa1dee9b349ac7d23a25ded29210dc6a73b24
yeonjin1357 <yeonjin1357@naver.com>
2024-03-25 18:00:51 +0900

    수정

--- app/page.js
+++ app/page.js
@@ -1,10 +1,5 @@
-import Map from "@/components/map/map";
 import classes from "./page.module.css";
 
 export default function Home() {
-  return (
-    <main className={classes.main}>
-      <Map></Map>
-    </main>
-  );
+  return <main className={classes.main}></main>;
 }
